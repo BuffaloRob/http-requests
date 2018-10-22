e9d57ce7d92eaedb51401ebd64360ab591189657
BuffaloRob <peckham.rob@gmail.com>
2018-10-22 10:37:57 -0400

    + postId to state, + postSelectedHandler, + ID prop to FullPost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -10,6 +10,7 @@ class Blog extends Component {
 
     state = {
         posts: [],
+        selectedPostId: null,
     }
 
     componentDidMount() {
@@ -27,9 +28,18 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>;
+            return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author} 
+                clicked={() => this.postSelectedHandler(post.id)}
+            />;
         })
         return (
             <div>
@@ -37,7 +47,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
