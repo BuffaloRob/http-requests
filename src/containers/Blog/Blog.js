d74f80a3ffe293bfbb35d7f425bcb957b30cd416
BuffaloRob <peckham.rob@gmail.com>
2018-10-30 15:21:06 -0400

    Blog: +- default Posts route new path is '/posts/' instead of '/'

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,7 +4,6 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -12,29 +11,23 @@ class Blog extends Component {
             <header>
               <nav>
                 <ul>
-                  <li><NavLink 
-                    to="/" 
-                    exact
-                    activeClassName="my-active"
-                    activeStyle={{
-                        color: '#fa923f',
-                        textDecoration: 'underline'
-                    }}
-                    >Posts</NavLink></li>
-                  <li><NavLink to={{
-                      pathname: '/new-post',
-                      hash: '#submit',
-                      search: '?quick-submit=true'
-                  }}>New Post</NavLink></li>
+                  <li>
+                    <NavLink to="/posts/" exact activeClassName="my-active" activeStyle={{ color: "#fa923f", textDecoration: "underline" }}>
+                      Posts
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to={{ pathname: "/new-post", hash: "#submit", search: "?quick-submit=true" }}>
+                      New Post
+                    </NavLink>
+                  </li>
                 </ul>
               </nav>
             </header>
             <Switch>
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
+              <Route path="/new-post" component={NewPost} />
+              <Route path="/posts" component={Posts} />
             </Switch>
-            
           </div>;
     }
 }
